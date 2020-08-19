9bef79aada54704ee68b688ae9ba55d67a692d2f
katejavonillo <69914728+katejavonillo@users.noreply.github.com>
2020-08-19 12:20:53 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/****************
+Hello.java
+******************
+Kate Javonillo
+08/18/2020
+CMSC 255 C90
+******************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("<Kate Javonillo>");
+	System.out.println("<Lab 01>");
+	System.out.println("<Hello.java>");
+	System.out.println("<08/18/2020>");
+	System.out.println("<CMSC 255 C90>");
+	System.out.println();
+	}
+}
